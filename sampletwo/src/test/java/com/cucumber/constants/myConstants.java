ce71e44ec08a80e453f3a6c6a719ac39bf1832ca
shruthikakondla <shruthika.siri@gmail.com>
2020-02-26 20:33:55 +0000

    updated

--- sampletwo/src/test/java/com/cucumber/constants/myConstants.java
+++
@@ -1,8 +0,0 @@
-package com.cucumber.constants;
-
-public class myConstants {
-	
-	public static final String email_address = "shru.siri@gmail.com";
-	public static final String date_of_birth = "10/10/1988";
-
-}
